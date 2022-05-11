c88c842fb81760805b380bd69bde6a8df1b76ec2
girinssh <tngus5851@naver.com>
2022-05-11 15:16:29 +0900

    Implement OnCreateView with DataBindingUtil

--- app/src/main/java/com/capstone/galaxyknot/fragment/CollectorFragment.java
+++ app/src/main/java/com/capstone/galaxyknot/fragment/CollectorFragment.java
@@ -2,6 +2,7 @@ package com.capstone.galaxyknot.fragment;
 
 import android.os.Bundle;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,6 +10,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.capstone.galaxyknot.R;
+import com.capstone.galaxyknot.StateManager;
+import com.capstone.galaxyknot.databinding.CollectorFragmentBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,33 +20,15 @@ import com.capstone.galaxyknot.R;
  */
 public class CollectorFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private CollectorFragmentBinding binding;
 
     public CollectorFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CollectorFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static CollectorFragment newInstance(String param1, String param2) {
         CollectorFragment fragment = new CollectorFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,8 +37,7 @@ public class CollectorFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
@@ -61,6 +45,10 @@ public class CollectorFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_collector, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_collector, container, false);
+        binding.setFrag(this);
+        binding.setIsClassifier(StateManager.isNowClassifierState);
+
+        return binding.getRoot();
     }
 }
