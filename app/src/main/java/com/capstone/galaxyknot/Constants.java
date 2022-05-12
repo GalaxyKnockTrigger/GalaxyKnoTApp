4a9f92ff0b4fa08509f1d00acac03f1d5c39ba3e
girinssh <tngus5851@naver.com>
2022-05-13 03:10:11 +0900

    remove non-used constants.
    edit receiving time
    
    edit sound peak 5000 to 10000 for removing multi detection at one knock.

--- app/src/main/java/com/capstone/galaxyknot/Constants.java
+++ app/src/main/java/com/capstone/galaxyknot/Constants.java
@@ -4,17 +4,11 @@ import android.Manifest;
 import android.hardware.Sensor;
 
 public class Constants {
-    public static final int TYPE_ACCEL = Sensor.TYPE_LINEAR_ACCELERATION;
-    public static final int TYPE_GYRO = Sensor.TYPE_GYROSCOPE_UNCALIBRATED;
     public static final String[] permissions = {Manifest.permission.RECORD_AUDIO};
     public static final int RCODE = 10000;
-    public static final int RECEIVING_TIME = 1000;
+    public static final int RECEIVING_TIME = 80;
     public static final String URL = "https://nas.splo2t.com";
-    public static final String TEST_URL = "https://nas.splo2t.com/class/";
-
     public static final int PORT = 9999;
-
     public static final int AUDIO_SAMP_RATE = 48000;
-
-    public static final short TH_SOUND_PEAK = 5000;
+    public static final short TH_SOUND_PEAK = 10000;
 }
